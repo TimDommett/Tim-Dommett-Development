bb39e61b0d18e1cce0b2769910103d504da366ca
Tim Dommett <timdom11@gmail.com>
2019-04-11 18:35:32 +0200

    added different layers to home hero image

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+// Moving hero image
 var lFollowX = 0,
     lFollowY = 0,
     x = 0,
@@ -16,6 +18,7 @@ function moveBackground() {
     'transform': translate
   });
 
+
   window.requestAnimationFrame(moveBackground);
 }
 
@@ -30,6 +33,39 @@ $(window).on('mousemove click', function(e) {
 
 moveBackground();
 
+// top hero image mover
+
+var lFollowXone = 0,
+    lFollowYone = 0,
+    xone = 0,
+    yone = 0,
+    frictionone = 1 / 30;
+
+function moveBackgroundOne() {
+  xone += (lFollowXone - xone) * frictionone;
+  yone += (lFollowYone - yone) * frictionone;
+
+  translate = 'translate(' + xone + 'px, ' + yone + 'px) scale(1.1)';
+
+  $('.bg-one').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackgroundOne);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseXone = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseYone = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowXone = (20 * lMouseXone) / 50; // 100 : 12 = lMouxeX : lFollow
+  lFollowYone = (10 * lMouseYone) / 100;
+
+});
+
+moveBackgroundOne();
 
 
 // scrolling skills div
@@ -83,5 +119,5 @@ function initSmoothScrolling(container,animation){
 $("#button").click(function() {
     $('html, body').animate({
         scrollTop: $("#scroll-stop").offset().top
-    }, 2000);
+    }, 1700);
 });
